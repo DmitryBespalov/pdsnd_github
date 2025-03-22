6247e50655d21ab7f65eaede0d85e72105fe91f9
Dmitrii Bespalov <284056+DmitryBespalov@users.noreply.github.com>
2025-03-22 15:30:17 +0100

    Added comments to main

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -237,14 +237,17 @@ def display_raw_data(city):
 
 def main():
     while True:
+        # Ask for data filters
         city, month, day = get_filters()
         df = load_data(city, month, day)
 
+        # Print user stats
         time_stats(df)
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
 
+        # Ask to print out the data
         display_raw_data(city)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
